8a5c478f45a70c1c47814b14e62b295b39e86bb1
Hairun Huang <hairunhuang@gmail.com>
2022-08-20 00:02:43 -0700

    axios

--- rainbow-tv/src/Banner.js
+++ rainbow-tv/src/Banner.js
@@ -3,6 +3,11 @@ import './Banner.css'
 import banner from './images/yellowBanner.png'
 
 function Banner() {
+    function trancate(string, n) {
+        return string.length > n ? string.substr(0, n-1) + "..." : string;
+    }
+
+
     return (
         <header className='banner' style={{
             backgroundSize: "cover",
@@ -19,7 +24,7 @@ function Banner() {
                     <button className='banner__button'>My List</button>
                 </div>
                 <h1 className='banner_description'>
-                    this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description this is a test description
+                   {trancate(`this is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test text`, 200)}
                 </h1>
             </div>
 
