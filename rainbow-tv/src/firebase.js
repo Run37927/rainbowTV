c29e29e881286b06611e079e6a2ccd04615ca68e
Hairun Huang <hairunhuang@gmail.com>
2022-08-19 20:33:30 -0700

    set up firebase

---
+++ rainbow-tv/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkMoaW21uzok_Im_CNNHaSkx56COf1pQg",
+    authDomain: "rainbow-tv-5ce1e.firebaseapp.com",
+    projectId: "rainbow-tv-5ce1e",
+    storageBucket: "rainbow-tv-5ce1e.appspot.com",
+    messagingSenderId: "804018644143",
+    appId: "1:804018644143:web:108d592f13d6615c57a05a"
+  };
