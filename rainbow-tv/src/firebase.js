28e04fcc0b9229db5658b71073d19ee67ff75fbe
Hairun Huang <hairunhuang@gmail.com>
2022-08-20 15:45:23 -0700

    connect to firestore

--- rainbow-tv/src/firebase.js
+++ rainbow-tv/src/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase';
-
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCkMoaW21uzok_Im_CNNHaSkx56COf1pQg",
--- rainbow-tv/src/screens/SignUpScreen.js
+++ rainbow-tv/src/screens/SignUpScreen.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import './SignUpScreen.css'
+import { auth } from "../firebase"
 
 
 function SignUpScreen() {
+  const emailRef = React.useRef(null);
+  const passwordRef = React.useRef(null);
+
+
   const register = (e) => {
     e.preventDefault();
+
+    auth.createUserWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then((authUser) => {
+      console.log(authUser)
+    }).catch(error => alert(error.message))
   }
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then( (authUser) => {
+      console.log(authUser)
+    }).catch(error => alert(error.message))
   }
 
   return (
@@ -16,11 +35,13 @@ function SignUpScreen() {
       <form>
         <h1>Sign In</h1>
         <input
+          ref={emailRef}
           placeholder='Email'
           type="email"
         />
 
         <input
+          ref={passwordRef}
           placeholder='Password'
           type="password"
         />
