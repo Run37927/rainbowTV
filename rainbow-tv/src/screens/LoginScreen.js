ce8aaf6e969961d401e69cf730d011fc99e3773c
Hairun Huang <hairunhuang@gmail.com>
2022-08-20 14:58:20 -0700

    sign up screen style

--- rainbow-tv/src/screens/LoginScreen.js
+++ rainbow-tv/src/screens/LoginScreen.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './LoginScreen.css'
 import logo from '../images/logo.png'
+import SignUpScreen from './SignUpScreen';
 
 function LoginScreen() {
+    const [signIn, setSignIn] = React.useState(false);
+
     return (
         <div className='loginScreen'>
             <div className='loginScreen__background'>
@@ -13,8 +16,8 @@ function LoginScreen() {
                 />
 
                 <button
+                    onClick={() => setSignIn(true)}
                     className='loginScreen__button'
-                // onClick={handleClick}
                 >Sign In
                 </button>
 
@@ -22,21 +25,28 @@ function LoginScreen() {
             </div>
 
             <div className='loginScreen__body'>
+                {signIn ? (
+                    <SignUpScreen />
+                ) : (
                 <>
                     <h1>Unlimited films, TV programs and more.</h1>
                     <h2>Watch anywhere. Cancel anytime.</h2>
                     <h3>Ready to watch? Enter your email to create or restart your membership</h3>
-                </>
-
-                <div className='loginScreen__input'>
+                    <div className='loginScreen__input'>
                     <form>
                         <input
                             type="email"
                             placeholder='Email Address'
                         />
-                        <button className='loginScreen__getStarted'>GET STARTED</button>
+                        <button
+                        onClick={() => setSignIn(true)}
+                        className='loginScreen__getStarted'>GET STARTED</button>
                     </form>
                 </div>
+                </>
+                )}
+
+           
             </div>
         </div>
     )
