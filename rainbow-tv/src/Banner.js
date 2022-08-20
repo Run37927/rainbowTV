41741e7ab210bd48ee96f3a9e5a267d53433e33e
Hairun Huang <hairunhuang@gmail.com>
2022-08-20 02:41:20 -0700

    http secure

--- rainbow-tv/src/Banner.js
+++ rainbow-tv/src/Banner.js
@@ -20,7 +20,6 @@ function Banner() {
         fetchData()
     }, [])
 
-    console.log(movie);
 
     // function trancate(string, n) {
     //     return string.length > n ? string.substr(0, n-1) + "..." : string;
