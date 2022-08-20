6c7d572ddee074908844c6d826404b22aa9d1a1a
Hairun Huang <hairunhuang@gmail.com>
2022-08-20 00:44:46 -0700

    pull in description

--- rainbow-tv/src/Banner.js
+++ rainbow-tv/src/Banner.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Banner.css'
-import banner from './images/yellowBanner.png'
 import axios from './axios'
 import requests from './Requests'
 
@@ -21,12 +20,11 @@ function Banner() {
         fetchData()
     }, [])
 
-    console.log(movie)
+    console.log(movie);
 
-
-    function trancate(string, n) {
-        return string.length > n ? string.substr(0, n-1) + "..." : string;
-    }
+    // function trancate(string, n) {
+    //     return string.length > n ? string.substr(0, n-1) + "..." : string;
+    // }
 
 
     return (
@@ -38,14 +36,14 @@ function Banner() {
 
             <div className='banner__contents'>
                 <h1 className='banner__title'>
-                    movie name
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className='banner_buttons'>
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>My List</button>
                 </div>
                 <h1 className='banner_description'>
-                   {trancate(`this is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test textthis is test text`, 200)}
+                   {movie?.overview}
                 </h1>
             </div>
 
