c2f2dd1b4ed0ea72567a5fc0110b0d63361835b1
Hairun Huang <hairunhuang@gmail.com>
2022-08-19 22:14:41 -0700

    scroll effect

--- rainbow-tv/src/Nav.js
+++ rainbow-tv/src/Nav.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import './Nav.css'
 import avatar from './images/avatar.png'
-import logo from './images/rainbowTv.png'
+import logo from './images/logo.png'
 
 function Nav() {
+    const [show, handleShow] = React.useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY >100) {
+            handleShow(true)
+        }else {
+            handleShow(false)
+        }
+    }
+
+    React.useEffect(() => {
+       window.addEventListener("scroll", transitionNavBar)
+       return () => window.removeEventListener('scroll', transitionNavBar)
+    }, [])
+
+
     return (
-        <div className='nav'>
+        <div className={`nav ${show && 'nav__bg'}`}>
             <div className='nav__contents'>
                 <img
                     className='nav__logo'
