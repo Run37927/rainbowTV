f55e8abcc25282817d79b24d5a9bbe5fcfe4ed44
Hairun Huang <hairunhuang@gmail.com>
2022-08-19 21:25:00 -0700

    add logo & avatar

--- rainbow-tv/src/Nav.js
+++ rainbow-tv/src/Nav.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Nav.css'
-
+import avatar from './images/avatar.png'
+import logo from './images/rainbowTv.png'
 
 function Nav() {
-  return (
-    <div className='nav'>
-        <h1>Nav bar here</h1>
-    </div>
-  )
+    return (
+        <div className='nav'>
+            <div className='nav__contents'>
+                <img
+                    className='nav__logo'
+                    src={logo}
+                    alt='logo'
+                />
+
+                <img
+                    className='nav__avatar'
+                    src={avatar}
+                    alt='avatar'
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Nav
